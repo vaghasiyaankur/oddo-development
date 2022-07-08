f632a6220ac5b3d1262b5048d245fa0ef8c68409
vikascodetrinity <vikas.codetrinity@gmail.com>
2022-07-08 14:33:21 -0500

    some changes in location page

--- Modules/Admin/Resources/views/location/index.blade.php
+++ Modules/Admin/Resources/views/location/index.blade.php
@@ -1,88 +1,89 @@
 @extends('layout::admin.master')
 
 @push('css')
-<!-- dropzone css -->
-{{-- <link rel="stylesheet" href="{{ asset('assets/Admin/assets/libs/dropzone/dropzone.css') }}" type="text/css" /> --}}
-{{-- <link href="https://unpkg.com/dropzone@6.0.0-beta.1/dist/dropzone.css" rel="stylesheet" type="text/css" /> --}}
-<link rel="stylesheet" href="https://unpkg.com/dropzone@5/dist/min/dropzone.min.css" type="text/css" />
+    <!-- dropzone css -->
+    {{-- <link rel="stylesheet" href="{{ asset('assets/Admin/assets/libs/dropzone/dropzone.css') }}" type="text/css" /> --}}
+    {{-- <link href="https://unpkg.com/dropzone@6.0.0-beta.1/dist/dropzone.css" rel="stylesheet" type="text/css" /> --}}
+    <link rel="stylesheet" href="https://unpkg.com/dropzone@5/dist/min/dropzone.min.css" type="text/css" />
 
-<style>
-    .Upload--img .dropzone {
-        min-height: 132px;
-        padding: 13px 17px;
-        border: 2px dashed #e9ebec;
-    }
+    <style>
+        .Upload--img .dropzone {
+            min-height: 132px;
+            padding: 13px 17px;
+            border: 2px dashed #e9ebec;
+        }
 
-    .gallery-light .edit-box {
-        opacity: 0;
-        transition: all 0.5S ease;
-        transform: translateY(50px);
-    }
+        .gallery-light .edit-box {
+            opacity: 0;
+            transition: all 0.5S ease;
+            transform: translateY(50px);
+        }
 
-    .gallery-light .edit-data-box:hover .edit-box {
-        opacity: 1;
-        transform: translateY(-20px);
-        z-index: 111;
-    }
+        .gallery-light .edit-data-box:hover .edit-box {
+            opacity: 1;
+            transform: translateY(-20px);
+            z-index: 111;
+        }
 
-    #gallery li#dropzone-preview-list {
-        list-style: none;
-    }
-</style>
+        #gallery li#dropzone-preview-list {
+            list-style: none;
+        }
+    </style>
 @endpush
 
 @section('content')
-<div class="page-content">
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="card" id="orderList">
-                <div class="card-header  border-0">
-                    <div class="row d-flex align-items-center">
-                        <div class="col-sm-4">
-                            <h5 class="card-title mb-0 ms-0 ms-sm-3  mb-3 mb-sm-0">Location</h5>
-                        </div>
-                        <div class="col-sm-8">
-                            <div class="d-flex align-items-center justify-content-evenly">
-                                <div class="search-box w-100">
-                                    <input type="text" class="form-control search"
-                                        placeholder="Search for Location, Country , status or something...">
-                                    <i class="ri-search-line search-icon"></i>
-                                </div>
-                                <div class="live-preview ms-4">
-                                    <button type="button" class="btn btn-success text-nowrap" data-bs-toggle="modal"
-                                        data-bs-target="#exampleModalgrid"><i class=" ri-add-line align-bottom me-1"></i>
-                                        Location Details
-                                    </button>
+    <div class="page-content">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card" id="orderList">
+                    <div class="card-header  border-0">
+                        <div class="row d-flex align-items-center">
+                            <div class="col-sm-4">
+                                <h5 class="card-title mb-0 ms-0 ms-sm-3  mb-3 mb-sm-0">Location</h5>
+                            </div>
+                            <div class="col-sm-8">
+                                <div class="d-flex align-items-center justify-content-evenly">
+                                    <div class="search-box w-100">
+                                        <input type="text" class="form-control search"
+                                            placeholder="Search for Location, Country , status or something...">
+                                        <i class="ri-search-line search-icon"></i>
+                                    </div>
+                                    <div class="live-preview ms-4">
+                                        <button type="button" class="btn btn-success text-nowrap" data-bs-toggle="modal"
+                                            data-bs-target="#exampleModalgrid"><i
+                                                class=" ri-add-line align-bottom me-1"></i>
+                                            Location Details
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="card-body border border-end-0 border-start-0 border-bottom-0">
-                    <div class="gallery-light">
-                        <div class="row">
-                        @include('admin::location.locationList')
+                    <div class="card-body border border-end-0 border-start-0 border-bottom-0">
+                        <div class="gallery-light">
+                            <div class="row">
+                                @include('admin::location.locationList')
+                            </div>
+                            <!--end row-->
                         </div>
-                        <!--end row-->
                     </div>
                 </div>
             </div>
+            <!--end col-->
         </div>
-        <!--end col-->
     </div>
-</div>
 
 
 
-<!-- End Page-content -->
+    <!-- End Page-content -->
 
-<!--facility create Modal start -->
-@include('admin::location.create')
-<!--facility create Modal end -->
+    <!--facility create Modal start -->
+    @include('admin::location.create')
+    <!--facility create Modal end -->
 
-<!--facility edit Modal start -->
-@include('admin::location.edit')
-<!--facility edit Modal end -->
+    <!--facility edit Modal start -->
+    @include('admin::location.edit')
+    <!--facility edit Modal end -->
 @endsection
 
 
--- Modules/Admin/Resources/views/location/locationList.blade.php
+++ Modules/Admin/Resources/views/location/locationList.blade.php
@@ -38,8 +38,9 @@
             </a>
         </div>
         <div class="box-content">
-            <div class="d-flex align-items-center mt-2">
-                <div class="flex-grow-1 text-muted"><a href="" class="text-body text-truncate">10 listed Disended</a>
+            <div class="d-flex align-items-center justify-content-between flex-wrap mt-2">
+                <div class="flex-grow-1 text-muted"><a href="" class="text-body text-truncate">10 listed
+                        Disended</a>
                 </div>
                 <div class="flex-shrink-0">
                     <div class="d-flex gap-3">
@@ -54,3 +55,63 @@
     </div>
 </div>
 <!--end col-->
+{{-- Animation Location Box --}}
+<div class="col-xl-2 col-lg-4 col-sm-6">
+    <div class="gallery-box edit-data-box card">
+        <div class="edit-box d-none">
+            <div class="dropdown"><button
+                    class="btn btn-white bg-success btn-sm dropdown position-absolute translate-middle rounded-pill"
+                    style="right:-8px;top: 42px;z-index:111;" type="button" data-bs-toggle="dropdown"
+                    aria-expanded="false"><i class="ri-more-fill text-white"></i></button>
+                <ul class="dropdown-menu dropdown-menu-end p-0" style="border-radius:8px;min-width: 4rem;">
+                    <li>
+                        <a class="dropdown-item amenityEdit" style="padding: 0.35rem 0.75rem;" href="javascript:;"
+                            data-bs-toggle="modal" data-bs-target="#exampleModalgrid" data-value=""><i
+                                class="ri-pencil-fill align-bottom me-2 text-muted"></i>
+                            Edit</a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item text-danger" style="padding: 0.35rem 0.75rem;" href="javascript:;"><i
+                                class="ri-delete-bin-fill align-bottom me-2 text-danger"></i>
+                            Delete</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="gallery-container">
+            <div class="loadingShow td-1">
+                <span></span>
+            </div>
+            <a class="image-popup d-none" href="assets/images/small/img-1.jpg" title="">
+                <img class="gallery-img img-fluid mx-auto"
+                    src="{{ asset('assets/Admin/assets/images/small/img-7.jpg') }}" alt="">
+                <div class="gallery-overlay d-flex justify-content-between">
+                    <h5 class="overlay-caption mb-1">New York</h5>
+                </div>
+            </a>
+        </div>
+        <div class="box-content">
+            <div class="d-flex align-items-center mt-2">
+                <div class="flex-grow-1 text-muted">
+                    <div class="loadingShow td-3">
+                        <span></span>
+                    </div>
+                    <a href="" class="text-body text-truncate d-none">10 listed
+                        Disended</a>
+                </div>
+                <div class="flex-shrink-0">
+                    <div class="loadingShow td-3">
+                        <span></span>
+                    </div>
+                    <div class="d-flex gap-3 d-none">
+                        <label class="mb-0" for="">Feature</label>
+                        <div class="form-check form-switch form-switch-success ms-1">
+                            <input class="form-check-input" type="checkbox" role="switch" id="SwitchCheck3"
+                                checked="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
